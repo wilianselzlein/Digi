090fad1746f7d0a091c7d388285a4e69deceaa89
Wilian Selzlein <wilianselzlein@gmail.com>
2013-07-08 21:01:46 -0300

    Melhorias no balcao

--- push.bat
+++ push.bat
@@ -1,3 +1,5 @@
+d:\Delphi\upx301w\upx.exe d:\delphi\Digi\Win32\Debug\Digi.exe
+d:\Programas\Upx301w\upx307w\upx.exe d:\Wilian\Digi\Win32\Debug\Digi.exe
 git add --all
 git commit -m "Melhorias no balcao"
 git push origin master
